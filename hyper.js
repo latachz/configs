4831cd7c664689bb093f529b1632127f840bea97
Kacper Latuszewski <16508923+latachz@users.noreply.github.com>
2018-08-06 15:55:44 +0200

    Update hyper.js

--- hyper.js
+++ hyper.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 16,
+    fontSize: 17,
 
     // font family with optional fallbacks
     fontFamily: '"Monaco for Powerline", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
